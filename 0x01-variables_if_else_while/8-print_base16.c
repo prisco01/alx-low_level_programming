27c24cc0c676387f7f4de08072fc11360b68e16f
prisco01 <dapo.olagunju@gmail.com>
2022-10-14 17:31:13 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,12 +10,15 @@ int main(void)
 	int num;
 	char letter;
 
-	for (num = 0; num <= 9; num++)
+	for (num = 0; num < 10; num++)
+	{
 		putchar(num);
+	}
 
 	for (letter = 'a'; letter <= 'f'; letter++)
+	{
 		putchar(letter);
-
+	}
 	putchar('\n');
 
 	return (0);
