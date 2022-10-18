72a784968699ef71b1715fd4ea810e074572e33f
prisco01 <dapo.olagunju@gmail.com>
2022-10-18 10:15:24 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-* print_alphabet -  prints 10 times the alphabet, in lowercase.
+* print_alphabet_x10 -  prints 10 times the alphabet, in lowercase.
 */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 	int count = 0;
 	char letter;
