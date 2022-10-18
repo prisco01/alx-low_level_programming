9171ecd05e93164649d96f27f19c37043ec50b7e
prisco01 <dapo.olagunju@gmail.com>
2022-10-18 14:36:39 -0700

    Task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -21,11 +21,20 @@ void times_table(void)
 
 			prod = num * mult;
 
-			if (prod <= 9)
-				_putchar(' ');
-			else
+			if ((prod / 10) > 0)
+			{
 				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((prod / 10) + '0');
+			if (mult < 9)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
 		}
 		_putchar('\n');
 	}
