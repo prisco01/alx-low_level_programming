7693a63e9f8ebee502868ca7b63230775a68fb11
prisco01 <dapo.olagunju@gmail.com>
2022-10-20 07:17:32 -0700

    Task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,7 +12,7 @@ int main(void)
 
 	printf("Infinite loop incoming :(\n");
 
-	i = 0;
+
 
 	/* while (i < 10)*/
 	/* {*/
@@ -22,6 +22,5 @@ int main(void)
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
-
 }
 
