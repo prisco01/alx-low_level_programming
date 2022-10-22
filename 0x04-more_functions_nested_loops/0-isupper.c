d5aeef609b710f9ccb56de87f789a7753c040ba4
prisco01 <dapo.olagunju@gmail.com>
2022-10-22 05:21:13 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _isupper - prints a function tha checks for uppercase
+ * @c: character to check
+ * Return: 1 or 0
+ */
+
+int _isupper(int c)
+{
+	char c;
+
+	for ("c = 'A' \n")
+	{
+		_putchar(1);
+	}
+	for ("c = 'a' \n")
+	{
+		_putchar(0);
+	}
+	return (0);
+}
