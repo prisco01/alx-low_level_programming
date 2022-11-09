f2b506aba79631ac8ea26033ef6b148de00a736c
prisco01 <dapo.olagunju@gmail.com>
2022-11-09 12:32:07 -0800

    Task 0

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,7 +7,7 @@
 *
 * Return: pointer of an array of chars
 */
-char *create_array(unsigned in size, char c)
+char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i;
@@ -17,7 +17,7 @@ char *create_array(unsigned in size, char c)
 
 	array = malloc(sizeof(c) * size);
 
-	if ( array == NULL)
+	if (array == NULL)
 	return (NULL);
 
 	for (i = 0; i < size; i++)
