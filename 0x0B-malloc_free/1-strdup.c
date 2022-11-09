52b614ffc82be129439fabd39c6e1efed86d6f86
prisco01 <dapo.olagunju@gmail.com>
2022-11-09 13:08:10 -0800

    Task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,19 +9,21 @@
 char *_strdup(char *str)
 {
 	char *strout;
-	unsigned int i, j;
+	unsigned int i, j = 0;
 
 	if (str == NULL)
 	return (NULL);
 
-	for (i = 0; str[i] != '\0'; i++)
-	strout = (char *)malloc(sizeof(char) * (i + 1));
+	while (str[j] != '\0')
+		j++;
 
+	strout = (char *)malloc((sizeof(char) * j) + 1);
 	if (strout == NULL)
-	return (NULL);
+		return (NULL);
 
-	for (j = 0; j <= i; j++)
-	strout[j] = str[j];
+	for (i = 0; i < j; i++)
+		strout[i] = str[i];
+	strout[j] = '\0';
 
 	return (strout);
 }
