e45fa233abc77a948d7c6ceb2f911751c3e54f44
prisco01 <dapo.olagunju@gmail.com>
2022-11-15 10:53:15 -0800

    Task 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,8 +7,9 @@
  * @age: age of dog
  * @owner: pointer to owner of dog
  *
- * Return: void
+ * Return: Always 0
  */
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
